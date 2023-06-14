35466c811852b770177fac1d822038d94595cbae
Guigo1008 <guigo@eq.ufrj.br>
2023-06-14 18:55:18 -0300

    Lista 2

---
+++ CCA_Lista2/questao_6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(int argc, char** argv) {
+
+}
+
+double media_aritmetica(double array_valores){
+    unsigned int tamanho_array = array_valores.len();
+    for (int i; i < tamanho_array; i++) {
+
+    }
+    return
+}
