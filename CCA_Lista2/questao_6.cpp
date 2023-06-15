6a7c915d62764b137211219c6645c76abfce7b4f
Guigo1008 <guigo@eq.ufrj.br>
2023-06-14 21:33:39 -0300

    Lista 2 - com questao 7

--- CCA_Lista2/questao_6.cpp
+++ CCA_Lista2/questao_6.cpp
@@ -2,14 +2,48 @@
 #include <cmath>
 
 using namespace std;
+
+double* inicializa_array();
+unsigned int tamanho;
+
 int main(int argc, char** argv) {
+    double soma1 = 0, soma2 = 0, media, desvio_padrao;
+    auto* array_valores = inicializa_array();
 
-}
-
-double media_aritmetica(double array_valores){
-    unsigned int tamanho_array = array_valores.len();
-    for (int i; i < tamanho_array; i++) {
-
+    // Calculando a media
+    for(int i = 0; i < tamanho; i++){
+        soma1 += array_valores[i];
     }
-    return
+    media = soma1/tamanho;
+
+    // Calculando o Desvio Padrao
+    for(int i = 0; i < tamanho; i++){
+        soma2 += pow(array_valores[i], 2.);
+    }
+    desvio_padrao = sqrt((tamanho * soma2 - pow(soma1, 2.))
+            / tamanho / (tamanho-1));
+
+    cout << "A media encontrada para esses valores eh: " << media << "\n";
+    cout << "O desvio padrao encontrado para esses valores eh: " << desvio_padrao << "\n";
 }
+
+double* inicializa_array() {
+    string valores;
+    cout << "Quantos valores voce deseja inserir? ";
+    cin >> tamanho;
+    auto* array_valores = new double[tamanho];
+
+    for(int i = 0; i < tamanho; i++){
+        cout << "Insira os valores, um a um, que voce deseja inserir: ";
+        cin >> array_valores[i];
+        if(array_valores[i] < 0) {
+            cout << "Infelizmente, neste codigo, valores negativos nao sao permitidos." << endl;
+            return 0;
+        }
+    }
+
+    return array_valores;
+}
+
+
+
